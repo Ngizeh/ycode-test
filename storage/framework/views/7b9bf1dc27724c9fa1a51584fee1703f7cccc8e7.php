dd3619bf7ed04ea0d1803c485134d6d0846e60a6
Ngizeh Mwas <edngize@gmail.com>
2021-03-31 13:59:08 +0300

    Optimized Cache

---
+++ storage/framework/views/7b9bf1dc27724c9fa1a51584fee1703f7cccc8e7.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="icon" href="data:;base64,=">
+    </head>
+    <body class="py-20">
+        <div id="app">
+            <people-component :team = "<?php echo e($team); ?>"></people-component>
+        </div> 
+        <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+    </body>
+</html><?php /**PATH /home/ngizeh/Projects/Javascript/ycode-test/resources/views/people.blade.php ENDPATH**/ ?>
